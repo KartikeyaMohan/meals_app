7d4f245bcdd9861c85c0c4cb1527481ac210c6c1
Kartikeya <33117118+KartikeyaMohan@users.noreply.github.com>
2023-02-14 23:22:54 +0530

    Feature/drawer (#2)
    * Bottom navigation added
    
    * Drawer added

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,27 +9,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DailyMeals'),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20
-        ),
-        children: CATEGORIES.map(
-          (category) =>
-            CategoryItem(
-              id: category.id,
-              title: category.title, 
-              color: category.color
-            )
-          ).toList(),
-      ),
+      children: CATEGORIES.map(
+        (category) =>
+          CategoryItem(
+            id: category.id,
+            title: category.title, 
+            color: category.color
+          )
+        ).toList(),
     );
   }
 }
